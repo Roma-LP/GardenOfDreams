49b1a3cc370c173beb51c708d0ac6a12e88e3129
Roma <rlepesh@mail.ru>
2025-06-13 07:40:36 +0300

    feat: add inventory UI

---
+++ Assets/_GardenOfDreams/Scripts/UI/ButtonClickHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class ButtonClickHandler : MonoBehaviour
+{
+    [SerializeField] private Button _trackingButton;
+
+    public event Action OnClicked;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        throw new NotImplementedException();
+    }
+}
